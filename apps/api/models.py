6fd44de80b33b1fe54f6705c87373d25bc048188
dvuud <baltabaevdavud88@gmail.com>
2024-04-24 18:21:41 +0600

    Гзвфеуы

--- apps/api/models.py
+++ apps/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import RegexValidator 
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
 # My model
 phone_regex = RegexValidator(regex=r'^\+996\d{9}$', message="Номер телефона необходимо ввести в формате: '+996xxxxxxxxx'.")
 class User(AbstractUser):
@@ -14,7 +14,7 @@ class User(AbstractUser):
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
     
-user = get_user_model()
+# user = get_user_model()
 class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todo_users', verbose_name='Пользователь')
     title = models.CharField(max_length=100, unique=True, verbose_name='Заголовок')
